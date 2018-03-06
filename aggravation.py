dd2765dbc12019df03529c7b1c704f198f859e69
James Ickes <james.ickes@mkcorp.com>
2018-03-06 13:18:34 -0600

    saving main file

---
+++ aggravation.py
@@ -0,0 +1,139 @@
+# Aggravation Game
+# By The Dude
+#
+# Released under a "Simplified BSD" license
+
+import random, pygame, sys
+from pygame.locals import *
+
+# How many spaces/pixels wide & tall is the board?
+# 27 spaces tall with one filled in every other
+# 30 spaces wide with one filled in every other
+''' 
+ Quick reference of coordinates of board
+  0123456789012345678901234567890
+0[                               ]
+1[           # # # # #           ]
+2[   #       #   #   #       #   ]
+3[     #     #   #   #     #     ]
+4[       #   #   #   #   #       ]
+5[         # #   #   # #         ]
+6[ # # # # # #       # # # # # # ]
+7[ #                           # ]
+8[ # # # # #     #     # # # # # ]
+9[ #                           # ]
+0[ # # # # # #       # # # # # # ]
+1[         # #   #   # #         ]
+2[       #   #   #   #   #       ]
+3[     #     #   #   #     #     ]
+4[   #       #   #   #       #   ]
+5[           # # # # #           ]
+6[                               ]
+'''
+
+BOARD_TEMPLATE =    ['...............................',
+                     '...........#.#.#.#.#...........',
+                     '...#.......#...#...#.......#...',
+                     '.....#.....#...#...#.....#.....',
+                     '.......#...#...#...#...#.......',
+                     '.........#.#...#...#.#.........',
+                     '.#.#.#.#.#.#.......#.#.#.#.#.#.',
+                     '.#...........................#.',
+                     '.#.#.#.#.#.....#.....#.#.#.#.#.',
+                     '.#...........................#.',
+                     '.#.#.#.#.#.#.......#.#.#.#.#.#.',
+                     '.........#.#...#...#.#.........',
+                     '.......#...#...#...#...#.......',
+                     '.....#.....#...#...#.....#.....',
+                     '...#.......#...#...#.......#...',
+                     '...........#.#.#.#.#...........',
+                     '...............................']
+
+FPS = 30 # frames per second, the general speed of the program
+WINDOWWIDTH = 640 # size of window's width in pixels
+WINDOWHEIGHT = 480 # size of windows' height in pixels
+REVEALSPEED = 8 # speed boxes' sliding reveals and covers
+BOXSIZE = 10 # size of box height & width in pixels (using box size for now to be the board spot marker)
+GAPSIZE = 10 # size of gap between boxes in pixels
+BOARDWIDTH = 30 # number of columns of icons
+BOARDHEIGHT = 16 # number of rows of icons
+BLANK = '.'
+#assert (BOARDWIDTH * BOARDHEIGHT) % 2 == 0, 'Board needs to have an even number of boxes for pairs of matches.'
+XMARGIN = int((WINDOWWIDTH - (BOARDWIDTH * (BOXSIZE + GAPSIZE))) / 2)
+YMARGIN = int((WINDOWHEIGHT - (BOARDHEIGHT * (BOXSIZE + GAPSIZE))) / 2)
+
+#            R    G    B
+GRAY     = (100, 100, 100)
+NAVYBLUE = ( 60,  60, 100)
+WHITE    = (255, 255, 255)
+RED      = (255,   0,   0)
+GREEN    = (  0, 255,   0)
+BLUE     = (  0,   0, 255)
+YELLOW   = (255, 255,   0)
+ORANGE   = (255, 128,   0)
+PURPLE   = (255,   0, 255)
+CYAN     = (  0, 255, 255)
+
+BGCOLOR = NAVYBLUE
+LIGHTBGCOLOR = GRAY
+BOXCOLOR = WHITE
+HIGHLIGHTCOLOR = BLUE
+
+DONUT = 'donut'
+SQUARE = 'square'
+DIAMOND = 'diamond'
+LINES = 'lines'
+OVAL = 'oval'
+
+ALLCOLORS = (RED, GREEN, BLUE, YELLOW, ORANGE, PURPLE, CYAN)
+ALLSHAPES = (DONUT, SQUARE, DIAMOND, LINES, OVAL)
+#assert len(ALLCOLORS) * len(ALLSHAPES) * 2 >= BOARDWIDTH * BOARDHEIGHT, "Board is too big for the number of shapes/colors defined."
+
+def main():
+    global FPSCLOCK, DISPLAYSURF
+    pygame.init()
+    FPSCLOCK = pygame.time.Clock()
+    DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
+
+    pygame.display.set_caption('Aggravation')
+
+    DISPLAYSURF.fill(BGCOLOR)
+
+    while True: # main game loop
+        mouseClicked = False
+
+        DISPLAYSURF.fill(BGCOLOR) # drawing the window
+        drawBoard()
+
+        for event in pygame.event.get(): # event handling loop
+            if event.type == QUIT or (event.type == KEYUP and event.key == K_ESCAPE):
+                pygame.quit()
+                sys.exit()
+            elif event.type == MOUSEMOTION:
+                mousex, mousey = event.pos
+            elif event.type == MOUSEBUTTONUP:
+                mousex, mousey = event.pos
+                mouseClicked = True
+
+        # Redraw the screen and wait a clock tick.
+        pygame.display.update()
+        FPSCLOCK.tick(FPS)
+
+
+def drawBoard():
+    # Draws all of the boxes in their covered or revealed state.
+    for boxx in range(BOARDWIDTH):
+        for boxy in range(BOARDHEIGHT):
+            if BOARD_TEMPLATE[boxy][boxx] != BLANK:
+              left, top = leftTopCoordsOfBox(boxx, boxy)
+              # Draw a small box representing a game board spot
+              pygame.draw.rect(DISPLAYSURF, BOXCOLOR, (left, top, BOXSIZE, BOXSIZE))
+
+def leftTopCoordsOfBox(boxx, boxy):
+    # Convert board coordinates to pixel coordinates
+    left = boxx * (BOXSIZE + GAPSIZE) + XMARGIN
+    top = boxy * (BOXSIZE + GAPSIZE) + YMARGIN
+    return (left, top)
+
+if __name__ == '__main__':
+    main()
